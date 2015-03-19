0d3aa2844c266713cd963ab30156140c216dc908
Eric <eric.nobody@gmail.com>
2015-03-19 16:43:29 -0400

    Added KILL event to .cfg file

--- votingSystem/src/districtserver/DistrictServer.java
+++ votingSystem/src/districtserver/DistrictServer.java
@@ -11,6 +11,8 @@ import common.Service;
 public class DistrictServer extends Server {
 
 	private ConcurrentHashMap<String, String> users;
+	private ConcurrentHashMap<String, Integer> votesToUpdate;
+	private ConcurrentHashMap<String, Integer> totalVotes;
 	private static int MAIN_SERVER_PORT = 9090;
 	private Connector connector;
 	private Connection mainConnection;
@@ -21,6 +23,8 @@ public class DistrictServer extends Server {
 		connector = new Connector(this);
 		
 		this.users = new ConcurrentHashMap<String, String>();
+		this.votesToUpdate = new ConcurrentHashMap<String,Integer>();
+		this.totalVotes = new ConcurrentHashMap<String,Integer>();
 	}
 
 	public static void main(String[] args) {
@@ -51,6 +55,33 @@ public class DistrictServer extends Server {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+
+	public boolean recordVote(String vote){
+		if(votesToUpdate.containsKey(vote)){
+			votesToUpdate.put(vote, votesToUpdate.get(vote) + 1);
+			totalVotes.put(vote, votesToUpdate.get(vote) + 1);
+		}
+		else{
+			votesToUpdate.put(vote, 1);
+			totalVotes.put(vote, 1);
+		}
+		Service.logInfo(votesToUpdate.get(vote) + " votes for "+ vote);
+		return true;
+	}
+	
+	private boolean updateMainServer(){
+		//send the main server all the contents of votesToUpdate hashmap 
+		
+		
+		
+		//reset hashmap
+		for(String key: votesToUpdate.keySet()){
+			votesToUpdate.put(key, 0);
+		}
+		
+		return true;
 	}
 	
 	public ConcurrentHashMap<String, String> getUsers(){
